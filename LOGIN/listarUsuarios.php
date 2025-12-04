51acdb2aa6adf0d013916242436256d3aab07773
Camila <programmer666loser@gmail.com>
2025-12-04 15:51:36 -0300

    style na listagem e arquivo database

--- LOGIN/listarUsuarios.php
+++ LOGIN/listarUsuarios.php
@@ -2,24 +2,38 @@
     require_once "conn.php";
     $usuarios = get_usuarios();
 ?>
-<div class="container">
-    <h1>Lista de Usuários</h1>
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Login</th>
-            <th>Editar</th>
-        </tr>
-        <?php foreach ($usuarios as $usuario): ?>
-        <tr>
-            <td><?php echo htmlspecialchars($usuario['id']); ?></td>
-            <td><?php echo htmlspecialchars($usuario['nome']); ?></td>
-            <td><?php echo htmlspecialchars($usuario['email']); ?></td>
-            <td><a href="editarUsuario.php?id=<?php echo urlencode($usuario['id']); ?>">Editar</a></td>
-             <td><a href="deletarUsuario.php?id=<?php echo urlencode($usuario['id']); ?>">Deletar</a></td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
-    <a href="cadastrar.php">Cadastrar</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/lista.css">
+    <title>Usuarios</title>
+</head>
+<body>
+    <div class="container">
+    <div class="content">
+        <h2>Lista de Usuários</h2>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Editar</th>
+                <th>Deletar</th>
+            </tr>
+            <?php foreach ($usuarios as $usuario): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($usuario['id']); ?></td>
+                <td><?php echo htmlspecialchars($usuario['nome']); ?></td>
+                <td><?php echo htmlspecialchars($usuario['email']); ?></td>
+                <td><a href="editarUsuario.php?id=<?php echo urlencode($usuario['id']); ?>">Editar</a></td>
+                <td><a href="deletarUsuario.php?id=<?php echo urlencode($usuario['id']); ?>">Deletar</a></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+        <a href="cadastrar.php" class="button">Cadastrar</a>
         </div>
+    </div>
+</body>
+</html>
